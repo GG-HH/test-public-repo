236d130d4ae0dedbf28211757a6f218b1ef23a50
Bernad Flagorner <bernad.flagorner@protonmail.com>
2020-09-16 11:47:21 +0200

    add jazz music

---
+++ src/jazz.rb
@@ -0,0 +1,8 @@
+class Jazz
+
+	# this function is useful for gg-tech-departmentX
+	def self.listen_to_through_api(music)
+
+		ping_api(music, 'PMAK-5e3c52895351d62230f49163-123cbbbd012251996d81dc089150e29d31')
+	end
+end
